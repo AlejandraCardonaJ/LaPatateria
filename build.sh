06f6fdb3c0d5fd09a597da1dc0c62720f1eea023
AlejandraCardonaJ <alejandra_jara@hotmail.com>
2026-03-23 12:33:17 -0500

    Agregar carga forzada con variables de entorno

--- build.sh
+++ build.sh
@@ -10,3 +10,9 @@ python manage.py migrate
 python manage.py createsuperuser --no-input || true
 
 python manage.py shell < cargar_datos.py
+
+if [ "$FORCE_LOAD_DATA" = "true" ]; then
+    echo "Forzando carga de datos adicional..."
+    python manage.py loaddata app_carta.json --verbosity=2
+    python manage.py loaddata app_fidelizacion.json --verbosity=2
+fi
