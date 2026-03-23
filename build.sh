62a100ffdb7265ca23e199ac0e3001605259c944
AlejandraCardonaJ <alejandra_jara@hotmail.com>
2026-03-23 18:05:30 -0500

    Volviendo a cargar build.sh pero con conmdicional para verificar existencia de categoria

--- build.sh
+++ build.sh
@@ -10,7 +10,17 @@ python manage.py migrate
 python manage.py createsuperuser --no-input || true
 
 # Carga directa de datos de la carta
-echo "=== Cargando datos de la carta ==="
-python manage.py loaddata app_carta.json --verbosity=2
+# echo "=== Cargando datos de la carta ==="
+# python manage.py loaddata app_carta.json --verbosity=2
+
+# echo "=== Proceso de construcción completado ==="
+
+# Cargar datos solo si la tabla de categorías está vacía
+if python manage.py shell -c "from app_carta.models import Categoria; exit(not Categoria.objects.exists())"; then
+    echo "=== Ya hay categorías, no se cargarán datos ==="
+else
+    echo "=== Cargando datos de la carta ==="
+    python manage.py loaddata app_carta.json --verbosity=2
+fi
 
 echo "=== Proceso de construcción completado ==="
