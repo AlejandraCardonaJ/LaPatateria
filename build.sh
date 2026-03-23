7b5dbf8a02d70eebc281444eff1364a17e570306
AlejandraCardonaJ <alejandra_jara@hotmail.com>
2026-03-23 18:03:41 -0500

    Volviendo a descomentar en build.sh la linea con loaddata

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ python manage.py migrate
 python manage.py createsuperuser --no-input || true
 
 # Carga directa de datos de la carta
-# echo "=== Cargando datos de la carta ==="
-# python manage.py loaddata app_carta.json --verbosity=2
+echo "=== Cargando datos de la carta ==="
+python manage.py loaddata app_carta.json --verbosity=2
 
-# echo "=== Proceso de construcción completado ==="
+echo "=== Proceso de construcción completado ==="
